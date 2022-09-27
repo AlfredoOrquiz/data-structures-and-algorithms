ed2fb652313cd0cf88852b4b470c424ff90573df
Alfredo <fredy_rios0123@outlook.com>
2022-09-27 23:05:50 +0000

    Code Challenge 07

---
+++ javascript/401-code-challenges/code-challenge-07/LinkedList.js
@@ -0,0 +1,58 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    const newHead = new Node(value);
+    const currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      newHead.next = currentHead;
+    }
+    return newHead;
+  }
+
+  append(data) {
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = new Node(data);
+  }
+
+  kFromEnd(k) {
+    if (k < 1) {
+      return "Exception";
+    }
+    let current = this.head;
+    let previousValue = this.head;
+    let counter = 0;
+    for (let i = 0; i < k - 1; i++) {
+      current = current.next;
+      if (!current) {
+        return "Exception";
+      }
+    } while (current !== null) {
+      current = current.next;
+      counter++;
+      if (k === counter - 1){
+        previousValue = previousValue.next;
+      }
+
+    }
+    return previousValue.value;
+  }
+
+}
+
+module.exports = LinkedList;
---
+++ javascript/401-code-challenges/code-challenge-07/README.md
@@ -0,0 +1,11 @@
+# Challenge Summary
+What we want to hapen is whenever we submit a value, we want to return the value that is provided by the index, as shown on the white board.
+
+## Whiteboard Process
+![](../Images/Challenge-7.jpg)
+
+## Approach & Efficiency
+I know that I needed an if statement, so I started writting a for loop with the if statements.
+
+## Contributions
+I would like to bring contributions to Jordan Yamada and Jeffrey Smith.
